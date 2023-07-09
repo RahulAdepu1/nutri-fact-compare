be0c58aae22bc6aec970796c1cbc5918119a585b
RahulAdepu1 <67685274+RahulAdepu1@users.noreply.github.com>
2023-07-08 22:18:17 -0500

    added custom Protocols

---
+++ NutriFactCompare/Protocols/ColorProtocols.swift
@@ -0,0 +1,14 @@
+//
+//  ColorProtocols.swift
+//  NutriFactCompare
+//
+//  Created by Rahul Adepu on 7/8/23.
+//
+
+import SwiftUI
+
+protocol ColorThemeProtocol {
+    var primary: Color { get }
+    var secondary: Color { get }
+    var tertiary: Color { get }
+}
